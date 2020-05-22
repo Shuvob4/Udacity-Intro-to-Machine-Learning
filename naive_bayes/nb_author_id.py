5facd74ee596158d8bb10ce7159afc436d11fccc
Shuvo Barman <shuvobarman69@gmail.com>
2020-05-22 12:09:42 +0600

    Navie Bayes Classifier with SciKit Learn

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,6 +27,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+#Importing Packages
+from sklearn.naive_bayes import GaussianNB
+
+#Create Classifier
+clf = GaussianNB()
+
+#fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train,labels_train)
+print ("training time:",round(time()-t0, 3),"s")
+
+# use the trained classifier to predict labels for the test features
+t0 = time()
+pred = clf.predict(features_test)
+print ("Test time:",round(time()-t0, 3),"s")
+
+#predicting accuracy
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+
+
 
 #########################################################
 
